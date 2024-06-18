5a8e780fd98b4b54c65fb1ec6a282564c95f26c8
brnikita <branikita@gmail.com>
2024-06-18 19:04:00 +0300

    feat: Backend. Added database configuration to app modeule.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,28 @@
+// \wix-products-catalog\backend\src\app.module.ts
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from './config/config.service';
 
 @Module({
-  imports: [ConfigModule, ProductsModule],
+  imports: [
+    ConfigModule,
+    ProductsModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',   
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,  
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
