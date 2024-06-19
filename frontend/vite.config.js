174444e35e73a2b736947350de2a11d842390f72
brnikita <branikita@gmail.com>
2024-06-19 13:52:51 +0300

    fix: frontend. Updated node environment and ports for frontend part

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  server: {
+    port: 3000,
+    host: true,  // This ensures the server is accessible externally
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
