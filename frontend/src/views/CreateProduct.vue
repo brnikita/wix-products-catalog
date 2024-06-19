9becd2d76ff2104eb0837d216031e64c555a2760
brnikita <branikita@gmail.com>
2024-06-19 23:03:08 +0300

    feat: Added button "Back to Products"

--- frontend/src/views/CreateProduct.vue
+++ frontend/src/views/CreateProduct.vue
@@ -1,18 +1,19 @@
 <template>
-    <div>
-      <h1 class="text-2xl mb-4">Create Product</h1>
-      <ProductForm />
-    </div>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent } from 'vue';
-  import ProductForm from '@/components/ProductForm.vue';
-  
-  export default defineComponent({
-    name: 'CreateProduct',
-    components: {
-      ProductForm,
-    },
-  });
-  </script>
+  <div>
+    <router-link to="/" class="text-blue-500 hover:text-blue-700">← Back to Products</router-link>
+    <h1 class="text-2xl mb-4">Create Product</h1>
+    <ProductForm />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ProductForm from '@/components/ProductForm.vue';
+
+export default defineComponent({
+  name: 'CreateProduct',
+  components: {
+    ProductForm,
+  },
+});
+</script>
